2c98c2a4dcbb3c576c93b89a95983b5bee10e9f4
justindu1017 <34846425+justindu1017@users.noreply.github.com>
2021-09-13 23:40:36 +0800

    first commit

---
+++ script/script.js
@@ -0,0 +1,7 @@
+const grayOutToggle = () => {
+  document.querySelector("#coverr").classList.toggle("grayOut");
+};
+
+nightMode = () => {
+  document.querySelector("body").classList.toggle("dark");
+};
