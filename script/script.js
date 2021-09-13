7c1183ecf152658f96f9998aab07cfb3e3466d61
justindu1017 <34846425+justindu1017@users.noreply.github.com>
2021-09-14 01:13:36 +0800

    test mobile close

--- script/script.js
+++ script/script.js
@@ -1,7 +1,12 @@
 const grayOutToggle = () => {
-  document.querySelector("#coverr").classList.toggle("grayOut");
+  document.querySelector("#coverer").classList.toggle("grayOut");
 };
-
+const closeGrayOut = () => {
+  const coverer
+  if(document.querySelector("#coverer").classList.contains("grayOut")){
+    document.querySelector("#coverer").classList.toggle("grayOut")
+  }
+};
 nightMode = () => {
   document.querySelector("body").classList.toggle("dark");
 };
